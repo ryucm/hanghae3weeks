7673398a9b2cd3a63ad498968d593b92631c4afd
changwook lee <leechangwook92>
2022-10-17 23:50:42 +0900

    시작

--- week3/src/main/java/com/example/week3/controller/ArticleController.java
+++ week3/src/main/java/com/example/week3/controller/ArticleController.java
@@ -2,6 +2,7 @@ package com.example.week3.controller;
 
 import com.example.week3.service.ArticleService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/post")
 public class ArticleController {
     private final ArticleService postService;
+
+    // 시이작
 }
 
