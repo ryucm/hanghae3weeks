f6fe0332fada740750bd77c37954cbf9e24b9447
metroz1 <wlstjr132@gmail.com>
2022-10-18 14:37:50 +0900

    Member signup & login 구현

--- week3/src/main/java/com/example/week3/dto/request/MemberRequestDto.java
+++ week3/src/main/java/com/example/week3/dto/request/MemberRequestDto.java
@@ -1,4 +1,18 @@
 package com.example.week3.dto.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
+@Getter
+@AllArgsConstructor
 public class MemberRequestDto {
+
+    private String nickname;
+    private String password;
+    private String passwordConfirm;
+
+    public UsernamePasswordAuthenticationToken toAuthentication() {
+        return new UsernamePasswordAuthenticationToken(nickname, password);
+    }
 }
