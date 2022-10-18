28d4bb4d8412823edf15a4a6ea7bfb5d7a61c0aa
metroz1 <wlstjr132@gmail.com>
2022-10-18 12:30:56 +0900

    ResponseDto, Error 클래스 구현

--- week3/src/main/java/com/example/week3/service/MemberService.java
+++ week3/src/main/java/com/example/week3/service/MemberService.java
@@ -1,11 +1,31 @@
 package com.example.week3.service;
 
+import com.example.week3.dto.response.MemberResponseDto;
+import com.example.week3.dto.response.ResponseDto;
 import com.example.week3.repository.MemberRepository;
+import com.example.week3.repository.RefreshTokenRepository;
+import com.example.week3.security.TokenProvider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class MemberService {
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+
     private final MemberRepository memberRepository;
+
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    private final TokenProvider tokenProvider;
+
+    private final PasswordEncoder passwordEncoder;
+
+//    @Transactional
+//    public ResponseDto<MemberResponseDto>
 }
