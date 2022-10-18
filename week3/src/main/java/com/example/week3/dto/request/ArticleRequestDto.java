c7e514b9edf28cb85f9c7967a7624cc8310f0c29
changwook lee <leechangwook92>
2022-10-18 14:44:26 +0900

    임시저장

---
+++ week3/src/main/java/com/example/week3/dto/request/ArticleRequestDto.java
@@ -0,0 +1,9 @@
+package com.example.week3.dto.request;
+
+import lombok.Getter;
+
+@Getter
+public class ArticleRequestDto {
+    private String title;
+    private String content;
+}
--- week3/src/test/java/com/example/week3/Week3ApplicationTests.java
+++ week3/src/test/java/com/example/week3/Week3ApplicationTests.java
@@ -1,7 +1,11 @@
 package com.example.week3;
 
+import com.example.week3.entity.Article;
+import com.example.week3.entity.Member;
+import com.example.week3.repository.ArticleRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 @SpringBootTest
 class Week3ApplicationTests {
@@ -9,5 +13,6 @@ class Week3ApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+
 
 }
